14bd6ca1cff2af34ce5383430656c3342dc3deab
Aaron <AaronWatson2975@gmail.com>
2019-08-06 17:26:26 -0230

    Added Navigation, DateTime picker, and form to add a new event

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import EventList from './EventList';
+import React from "react";
+import EventList from "./EventList";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import EventForm from "./EventForm";
 
-export default function App() {
-  return (
-    <EventList />
-  );
-}
+const AppNavigator = createStackNavigator({
+  list: {
+    screen: EventList,
+    navigationOptions: () => ({
+      title: "Your Events"
+    })
+  },
+  form: {
+    screen: EventForm,
+    navigationOptions: () => ({
+      title: "Add"
+    })
+  }
+});
+
+export default createAppContainer(AppNavigator);
--- EventList.js
+++ EventList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { FlatList, Text, StyleSheet } from "react-native";
 import EventCard from "./EventCard";
+import ActionButton from "react-native-action-button";
 
 const styles = StyleSheet.create({
   list: {
@@ -32,15 +33,24 @@ class EventList extends Component {
     this.setState({ events });
   }
 
+  handleAddEvent = () => {
+    this.props.navigation.navigate("form");
+  };
+
   render() {
-    return (
+    return [
       <FlatList
         style={styles.list}
         data={this.state.events}
         renderItem={({ item }) => <EventCard event={item} />}
         keyExtractor={item => item.id}
+      />,
+      <ActionButton
+        key="fab"
+        onPress={this.handleAddEvent}
+        buttonColor="rgba(231, 76, 60, 1)"
       />
-    );
+    ];
   }
 }
 
